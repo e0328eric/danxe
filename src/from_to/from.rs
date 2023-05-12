33a31252718cde7ec3973a02cde37d5efacf9df6
Sungbae Jeong <almagest0328@gmail.com>
2023-05-12 14:42:40 +0900

    remove the code that used at the debug purpose

--- src/from_to/from.rs
+++ src/from_to/from.rs
@@ -129,7 +129,6 @@ where
 
         let mut output = Vec::with_capacity(10);
         for subbytes in bytes[1..array_end_idx].split(|b| *b == BYTES_SEPARATOR) {
-            println!("{subbytes:x?}");
             let (val, rest) = T::from_bytes(subbytes)?;
             if !rest.is_empty() {
                 bail!(danxe_err!(FromBytesErr::FailedToMakeVector));
